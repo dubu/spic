e556979176db11180bebf6298272caf58ff17167
DOO-HWAN, KWAK <kozazz@hanmail.net>
2025-12-24 13:19:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const feedEl = document.querySelector("#feed");
 const emptyEl = document.querySelector("#empty");
-const template = document.querySelector("#card-template");
+const template = document.querySelectorAll('script[type="text/template"]');
 
 // Parse script template to create element
 function createNodeFromTemplate() {
